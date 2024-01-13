6b4cba22015e85c919916210d069087ec9457780
adityamore7000 <adi.more7000@gmail.com>
2024-01-13 17:35:39 +0530

    integrate code, styling needs fix

---
+++ nexus/src/Teams.js
@@ -0,0 +1,254 @@
+import React from 'react'
+import { Wrapper } from './assets/wrappers/TeamPage'
+export default function Teams() {
+    return (
+        <Wrapper>
+            <div className="main">
+
+                <h1 className="h1">TEAMS PAGE</h1>
+
+                <div className="head1">
+                    <div className="team1">
+                        <div className="container">
+                            <div className="card front">
+                                <div className="image">
+                                    <img src="images/profile-img.png" alt="" className="profile-img" />
+                                    <div className="text">
+                                        <span className="name">LOREM IPSUM</span>
+                                    </div>
+                                </div>
+                                <div className="icon">
+                                    <a href="#" className="glink">
+                                        <i className="fa-brands fa-github fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                    <a href="#" className="llink">
+                                        <i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container">
+                            <div className="card front">
+                                <div className="image">
+                                    <img src="images/profile-img.png" alt="" className="profile-img" />
+                                    <div className="text">
+                                        <span className="name">ABCDE FGHIJ</span>
+                                    </div>
+                                </div>
+                                <div className="icon">
+                                    <a href="#" className="glink">
+                                        <i className="fa-brands fa-github fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                    <a href="#" className="llink">
+                                        <i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container">
+                            <div className="card front">
+                                <div className="image">
+                                    <img src="images/profile-img.png" alt="" className="profile-img" />
+                                    <div className="text">
+                                        <span className="name">ABCDE FGHIJ</span>
+                                    </div>
+                                </div>
+                                <div className="icon">
+                                    <a href="#" className="glink">
+                                        <i className="fa-brands fa-github fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                    <a href="#" className="llink">
+                                        <i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container">
+                            <div className="card front">
+                                <div className="image">
+                                    <img src="images/profile-img.png" alt="" className="profile-img" />
+                                    <div className="text">
+                                        <span className="name">ABCDE FGHIJ</span>
+                                    </div>
+                                </div>
+                                <div className="icon">
+                                    <a href="#" className="glink">
+                                        <i className="fa-brands fa-github fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                    <a href="#" className="llink">
+                                        <i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+                <div className="head1">
+                    <div className="team1">
+                        <div className="container">
+                            <div className="card front">
+                                <div className="image">
+                                    <img src="images/profile-img.png" alt="" className="profile-img" />
+                                    <div className="text">
+                                        <span className="name">ABCDE FGHIJ</span>
+                                    </div>
+                                </div>
+                                <div className="icon">
+                                    <a href="#" className="glink">
+                                        <i className="fa-brands fa-github fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                    <a href="#" className="llink">
+                                        <i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container">
+                            <div className="card front">
+                                <div className="image">
+                                    <img src="images/profile-img.png" alt="" className="profile-img" />
+                                    <div className="text">
+                                        <span className="name">ABCDE FGHIJ</span>
+                                    </div>
+                                </div>
+                                <div className="icon">
+                                    <a href="#" className="glink">
+                                        <i className="fa-brands fa-github fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                    <a href="#" className="llink">
+                                        <i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container">
+                            <div className="card front">
+                                <div className="image">
+                                    <img src="images/profile-img.png" alt="" className="profile-img" />
+                                    <div className="text">
+                                        <span className="name">ABCDE FGHIJ</span>
+                                    </div>
+                                </div>
+                                <div className="icon">
+                                    <a href="#" className="glink">
+                                        <i className="fa-brands fa-github fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                    <a href="#" className="llink">
+                                        <i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container">
+                            <div className="card front">
+                                <div className="image">
+                                    <img src="images/profile-img.png" alt="" className="profile-img" />
+                                    <div className="text">
+                                        <span className="name">ABCDE FGHIJ</span>
+                                    </div>
+                                </div>
+                                <div className="icon">
+                                    <a href="#" className="glink">
+                                        <i className="fa-brands fa-github fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                    <a href="#" className="llink">
+                                        <i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+                <div className="head1">
+                    <div className="team1">
+                        <div className="container">
+                            <div className="card front">
+                                <div className="image">
+                                    <img src="images/profile-img.png" alt="" className="profile-img" />
+                                    <div className="text">
+                                        <span className="name">ABCDE FGHIJ</span>
+                                    </div>
+                                </div>
+                                <div className="icon">
+                                    <a href="#" className="glink">
+                                        <i className="fa-brands fa-github fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                    <a href="#" className="llink">
+                                        <i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container">
+                            <div className="card front">
+                                <div className="image">
+                                    <img src="images/profile-img.png" alt="" className="profile-img" />
+                                    <div className="text">
+                                        <span className="name">ABCDE FGHIJ</span>
+                                    </div>
+                                </div>
+                                <div className="icon">
+                                    <a href="#" className="glink">
+                                        <i className="fa-brands fa-github fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                    <a href="#" className="llink">
+                                        <i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container">
+                            <div className="card front">
+                                <div className="image">
+                                    <img src="images/profile-img.png" alt="" className="profile-img" />
+                                    <div className="text">
+                                        <span className="name">ABCDE FGHIJ</span>
+                                    </div>
+                                </div>
+                                <div className="icon">
+                                    <a href="#" className="glink">
+                                        <i className="fa-brands fa-github fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                    <a href="#" className="llink">
+                                        <i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="container">
+                            <div className="card front">
+                                <div className="image">
+                                    <img src="images/profile-img.png" alt="" className="profile-img" />
+                                    <div className="text">
+                                        <span className="name">ABCDE FGHIJ</span>
+                                    </div>
+                                </div>
+                                <div className="icon">
+                                    <a href="#" className="glink">
+                                        <i className="fa-brands fa-github fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                    <a href="#" className="llink">
+                                        <i className="fa-brands fa-linkedin fa-2xl" style={{ color: '#ffffff' }}></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
+        </Wrapper>
+    )
+}
